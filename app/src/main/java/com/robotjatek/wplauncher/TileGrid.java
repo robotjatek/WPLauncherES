281e8aa89a8a8550778a2183fa1025bc33632fdd
robotjatek <ati.szucs@hotmail.com>
2025-12-03 22:23:17 +0100

    Select tile

--- app/src/main/java/com/robotjatek/wplauncher/TileGrid.java
+++ app/src/main/java/com/robotjatek/wplauncher/TileGrid.java
@@ -19,17 +19,21 @@ public class TileGrid implements Page {
     private float _touchStartX = 0;
     private float _touchStartY = 0;
 
+    private Tile _selectedTile;
+
     private final Tile tile1 = new Tile(0, 0, 2, 2, ""); // 2x2 tile
     private final Tile tile2 = new Tile(0, 2, 1, 1, ""); // 1x1 tile
     private final Tile tile3 = new Tile(0, 4, 4, 2, ""); // Wide tile
     private final Tile tile4 = new Tile(0, 8, 4, 2, ""); // Wide tile
     private final Tile tile5 = new Tile(0, 20, 4, 4, ""); // 4x4 large tile far down
-    private final List<Tile> tiles = new ArrayList<>(List.of(tile1, tile2, tile3, tile4, tile5));
+    private final Tile tile6 = new Tile(2, 0, 2, 2, ""); // 2x2 tile
+    private final Tile tile7 = new Tile(2, 2, 2, 2, ""); // 2x2 tile
+    private final List<Tile> tiles = new ArrayList<>(List.of(tile1, tile2, tile3, tile4, tile5, tile6, tile7));
 
     private static final int COLUMNS = 4;
     private static final float TOP_MARGIN = 128;
     private static final float PAGE_PADDING_PX = 24;
-    private static final float TILE_GAP_PX = 18;
+    private static final float TILE_GAP_PX = 32;
 
     private final QuadRenderer renderer = new QuadRenderer();
 
@@ -52,6 +56,11 @@ public class TileGrid implements Page {
         Matrix.translateM(scrollMatrix, 0, 0, scroll.getScrollOffset(), 0);
 
         for (var t : tiles) {
+            // Do not render the selected tile here
+            if (t == _selectedTile) {
+                continue;
+            }
+
             Matrix.setIdentityM(modelMatrix, 0);
             Matrix.translateM(modelMatrix, 0, tileX(t), tileY(t), 0);
             Matrix.scaleM(modelMatrix, 0, tileWidth(t), tileHeight(t), 1);
@@ -59,19 +68,34 @@ public class TileGrid implements Page {
             Matrix.multiplyMM(modelMatrix, 0, scrollMatrix, 0, modelMatrix, 0);
             Matrix.multiplyMM(modelMatrix, 0, viewMatrix, 0, modelMatrix, 0);
 
-
-
             renderer.setColor(t.color);
             renderer.draw(projMatrix, modelMatrix);
         }
+
+        // render the selected tile
+        if (_selectedTile != null) {
+            var width = tileWidth(_selectedTile) * 1.05f;
+            var height = tileHeight(_selectedTile) * 1.05f;
+            var xDiff = (width - tileWidth(_selectedTile)) / 2;
+            var yDiff = (height - tileHeight(_selectedTile)) / 2;
+
+            Matrix.setIdentityM(modelMatrix, 0);
+            Matrix.translateM(modelMatrix, 0, tileX(_selectedTile) - xDiff, tileY(_selectedTile) - yDiff, 0f);
+            Matrix.scaleM(modelMatrix, 0, width, height, 1);
+            Matrix.multiplyMM(modelMatrix, 0, scrollMatrix, 0, modelMatrix, 0);
+            Matrix.multiplyMM(modelMatrix, 0, viewMatrix, 0, modelMatrix, 0);
+            renderer.setColor(new Color(0, 1, 0)); // TODO: remove this later
+            renderer.draw(projMatrix, modelMatrix);
+        }
+
     }
 
     public void onSizeChanged(int width, int height) {
-        float usableWidth = width - 2 * PAGE_PADDING_PX - (COLUMNS - 1) * TILE_GAP_PX;
+        var usableWidth = width - 2 * PAGE_PADDING_PX - (COLUMNS - 1) * TILE_GAP_PX;
         tileSizePx = usableWidth / COLUMNS;
 
-        float contentHeight = getContentHeight();
-        float min = Math.min(0, height - contentHeight - TOP_MARGIN);
+        var contentHeight = getContentHeight();
+        var min = Math.min(0, height - contentHeight - TOP_MARGIN);
         scroll.setBounds(min, TOP_MARGIN);
     }
 
@@ -128,14 +152,27 @@ public class TileGrid implements Page {
     }
 
     private void handleTap(float x, float y) {
+        var tappedTile = getTileAt(x, y);
+        if (_selectedTile != null) {
+            // Tapped same tile or empty space, deselect
+            if (tappedTile.isEmpty() || tappedTile.get() == _selectedTile) {
+                cancelSelection();
+            } else {
+                // Tap different tile
+                selectTile(tappedTile.get());
+            }
+            return;
+        }
+
         var r = new Random();
-        var tile = getTileAt(x, y);
-        tile.ifPresent(t -> t.color = new Color(r.nextFloat(), r.nextFloat(), r.nextFloat()));
+        tappedTile.ifPresent(t -> t.color = new Color(r.nextFloat(), r.nextFloat(), r.nextFloat()));
     }
 
     private void handleLongPress(float x, float y) {
         var tile = getTileAt(x, y);
-        tile.ifPresent(t -> t.color = new Color(0, 0, 1));
+        tile.ifPresent(this::selectTile);
+        // TODO: set grid to edit mode
+        // TODO: drag tile to a new position
     }
 
     private Optional<Tile> getTileAt(float x, float y) {
@@ -148,5 +185,15 @@ public class TileGrid implements Page {
 
             return x >= left && x <= right && y >= top && y <= bottom;
         }).findFirst();
+    }
+
+    private void selectTile(Tile tile) {
+        _selectedTile = tile;
+    }
+
+    private void cancelSelection() {
+        if (_selectedTile != null) {
+            _selectedTile = null;
+        }
     }
 }

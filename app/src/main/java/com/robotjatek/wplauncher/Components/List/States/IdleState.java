0672f248af29c82120975977e37afce8d11e8704
robotjatek <ati.szucs@hotmail.com>
2026-05-03 23:01:01 +0200

    Gesture refactor: context menu

--- app/src/main/java/com/robotjatek/wplauncher/Components/List/States/IdleState.java
+++ app/src/main/java/com/robotjatek/wplauncher/Components/List/States/IdleState.java
@@ -1,6 +1,7 @@
 package com.robotjatek.wplauncher.Components.List.States;
 
 import com.robotjatek.wplauncher.Components.List.ListView;
+import com.robotjatek.wplauncher.Gestures.LongPressGesture;
 import com.robotjatek.wplauncher.Gestures.ScrollGesture;
 import com.robotjatek.wplauncher.Gestures.TapGesture;
 
@@ -20,5 +21,11 @@ public class IdleState<T> extends BaseState<T> {
     public boolean handleScroll(ScrollGesture gesture) {
         _context.changeState(_context.SCROLL_STATE(gesture.getY()));
         return _context.handleGesture(gesture);
+    }
+
+    @Override
+    public boolean handleLongPress(LongPressGesture gesture) {
+        _context.changeState(_context.CONTEXT_MENU_STATE(gesture.getX(), gesture.getY()));
+        return _context.handleLongPress(gesture);
     }
 }
